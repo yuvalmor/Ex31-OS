7a54b2703041fe5baebe32a003d010b55cc197c5
yuvalmor <yuvalmor94@gmail.com>
2019-05-08 13:51:52 +0300

    finished after check in u2

--- ex31.c
+++ ex31.c
@@ -7,7 +7,10 @@
 #include <unistd.h>
 #include <ctype.h>
 
+#define ERROR (-1)
+#define SYSTEM_CALL_FAILURE (-1)
 #define END_OF_FILE 0
+#define INITIAL_VALUE 0
 #define FIRST_FILE_NAME 1
 #define NUMBER_BYTES_TO_READ 1
 #define IDENTITY 1
@@ -15,88 +18,103 @@
 #define DIFFERENT 2
 #define SAME 3
 #define NUMBER_OF_ELEMENTS 3
-#define SYSTEM_CALL_FAILURE (-1)
 #define DIFFERENCE_IN_ASCII 32
 #define A_VALUE_ASCII 65
 #define Z_VALUE_ASCII 122
 #define ERROR_MESSAGE "Error in system call\n"
+#define INPUT_ERROR "invalid input\n"
 
 void ExitProgram(){
     fprintf(stderr,ERROR_MESSAGE);
     exit(EXIT_FAILURE);
 }
 
-int IsIdentical(const char* firstFileName, const char* secondFileName){
+/**
+ *
+ * @param firstFilePath
+ * @param secondFilePath
+ * @return
+ */
+int IsIdenticalFiles(const char* firstFilePath, const char* secondFilePath){
+    bool flag = true;
     bool identity = true;
     bool different = false;
-    bool flag = true;
-    bool read1 = true;
-    bool read2 = true;
+    bool readFromFirstFile = true;
+    bool readFromSecondFile = true;
     int fd1,fd2;
-    ssize_t r1=0;
-    ssize_t r2=0;
-    char c1="";char c2 ="";
-    fd1 = open(firstFileName,O_RDONLY);
-    fd2 = open(secondFileName,O_RDONLY);
-    if(fd1==SYSTEM_CALL_FAILURE || fd2==SYSTEM_CALL_FAILURE){
+    ssize_t r1=INITIAL_VALUE;
+    ssize_t r2=INITIAL_VALUE;
+    char c1,c2;
+
+    fd1 = open(firstFilePath,O_RDONLY);
+    fd2 = open(secondFilePath,O_RDONLY);
+    if(fd1<INITIAL_VALUE || fd2<INITIAL_VALUE){
         ExitProgram();
     }
     while (flag && !different){
-        if(read1){
+        // Read the next char from the first file
+        if(readFromFirstFile){
             r1 = read(fd1,&c1,NUMBER_BYTES_TO_READ);
         }
-        if(read2){
+        // Read the next char from the second file
+        if(readFromSecondFile){
             r2 = read(fd2,&c2,NUMBER_BYTES_TO_READ);
         }
         // Checks failure in the system call
         if(r1 == SYSTEM_CALL_FAILURE || r2== SYSTEM_CALL_FAILURE){
-        ExitProgram();
+            ExitProgram();
         }
-        // Checks if the file ended
+        // Checks if one of the files ended
         if(r1==END_OF_FILE || r2==END_OF_FILE){
             flag = false;
             continue;
         }
         if(c1==c2) {
-            read1=true;
-            read2=true;
+            readFromFirstFile=true;
+            readFromSecondFile=true;
             continue;
         } else {
             identity = false;
             if(isspace(c1)){
-                read2 = false;
+                readFromSecondFile = false;
                 continue;
             }
             if(isspace(c2)){
-                read1 =false;
+                readFromFirstFile =false;
                 continue;
             }
             if(c1>=A_VALUE_ASCII && c2>=A_VALUE_ASCII && c1 <=Z_VALUE_ASCII
-            && c2<=Z_VALUE_ASCII && abs(c1-c2)== DIFFERENCE_IN_ASCII){
-                read1=true;
-                read2=true;
+               && c2<=Z_VALUE_ASCII && abs(c1-c2)== DIFFERENCE_IN_ASCII){
+                readFromFirstFile=true;
+                readFromSecondFile=true;
                 continue;
             }
             different=true;
         }
     }
-    //
-    if(r1==END_OF_FILE && r2!=END_OF_FILE){
+
+    /**
+     * In case that one of the files ended,and the other one didn't,
+     * It checks if the other file context assembled from white space only.
+     */
+    if(r1==END_OF_FILE && r2!=END_OF_FILE && !different){
+        identity=false;
         do{
             if(!isspace(c2)){
                 different=true;
+                break;
             }
             r2 = read(fd2,&c2,NUMBER_BYTES_TO_READ);
-            identity =false;
         }while (r2!=END_OF_FILE);
     }
-    if(r2==END_OF_FILE && r1!=END_OF_FILE){
+    if(r2==END_OF_FILE && r1!=END_OF_FILE && !different){
+        identity=false;
         do{
             if(!isspace(c1)){
                 different=true;
+                break;
             }
             r1 = read(fd1,&c1,NUMBER_BYTES_TO_READ);
-            identity =false;
         }while (r1!=END_OF_FILE);
 
     }
@@ -111,10 +129,9 @@ int IsIdentical(const char* firstFileName, const char* secondFileName){
 
 int main(int argc, char* argv[]) {
     if(argc!= NUMBER_OF_ELEMENTS){
-        printf("invalid input\n");
-        return -1;
+        printf(INPUT_ERROR);
+        return ERROR;
     }
-    int result = IsIdentical(argv[FIRST_FILE_NAME],argv[SECOND_FILE_NAME]);
-    printf("the result is = %d",result);
+    int result = IsIdenticalFiles(argv[FIRST_FILE_NAME],argv[SECOND_FILE_NAME]);
     return result;
 }
