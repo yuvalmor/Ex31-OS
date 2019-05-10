ba60b65368f1ce9fb4a1c12807183448d83d87ca
yuvalmor <yuvalmor94@gmail.com>
2019-05-10 17:45:43 +0300

    adding comments

--- ex31.c
+++ ex31.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <string.h>
 
 #define ERROR (-1)
 #define SYSTEM_CALL_FAILURE (-1)
@@ -24,35 +25,49 @@
 #define ERROR_MESSAGE "Error in system call\n"
 #define INPUT_ERROR "invalid input\n"
 
-void ExitProgram(){
-    fprintf(stderr,ERROR_MESSAGE);
-    exit(EXIT_FAILURE);
+
+/**
+ * The function EndProgram - write the error message (as a result from system call failure),
+ * To file descriptor 2 (stderr), and exit the program.
+ */
+void EndProgram(){
+    write(STDERR_FILENO,ERROR_MESSAGE,strlen(ERROR_MESSAGE));
+    exit(ERROR);
 }
 
 /**
- *
- * @param firstFilePath
- * @param secondFilePath
- * @return
+ * The function Compare - get two paths for two files,
+ * And compare between them.
+ * @param firstFilePath- path to the first file
+ * @param secondFilePath- path to the second file
+ * @return - 1 if they identical, 2 if they different and 3 if they similar.
  */
-int IsIdenticalFiles(const char* firstFilePath, const char* secondFilePath){
+int Compare(const char* firstFilePath, const char* secondFilePath){
+    // File directory to the files
+    int fd1,fd2;
+    // The character we read from each file
+    char c1,c2;
+    // Number of bytes that was read
+    ssize_t r1=INITIAL_VALUE;
+    ssize_t r2=INITIAL_VALUE;
     bool flag = true;
     bool identity = true;
     bool different = false;
     bool readFromFirstFile = true;
     bool readFromSecondFile = true;
-    int fd1,fd2;
-    ssize_t r1=INITIAL_VALUE;
-    ssize_t r2=INITIAL_VALUE;
-    char c1,c2;
-
+    // Open the files
     fd1 = open(firstFilePath,O_RDONLY);
     fd2 = open(secondFilePath,O_RDONLY);
+    // Check the system call open
     if(fd1<INITIAL_VALUE || fd2<INITIAL_VALUE){
-        ExitProgram();
+        EndProgram();
     }
+    /*
+     * Keeps running as long as the one of the files didn't end,
+     * And they don't different.
+     */
     while (flag && !different){
-        // Read the next char from the first file
+        // Read character from the first file
         if(readFromFirstFile){
             r1 = read(fd1,&c1,NUMBER_BYTES_TO_READ);
         }
@@ -60,21 +75,29 @@ int IsIdenticalFiles(const char* firstFilePath, const char* secondFilePath){
         if(readFromSecondFile){
             r2 = read(fd2,&c2,NUMBER_BYTES_TO_READ);
         }
-        // Checks failure in the system call
+        // Checks the system call read
         if(r1 == SYSTEM_CALL_FAILURE || r2== SYSTEM_CALL_FAILURE){
-            ExitProgram();
+            close(fd1);
+            close(fd2);
+            EndProgram();
         }
         // Checks if one of the files ended
-        if(r1==END_OF_FILE || r2==END_OF_FILE){
+        if(r1==END_OF_FILE||r2==END_OF_FILE){
             flag = false;
             continue;
         }
+        // Compare between the characters from the files
         if(c1==c2) {
             readFromFirstFile=true;
             readFromSecondFile=true;
             continue;
         } else {
             identity = false;
+            /**
+             * Checks if one of the character is white space,
+             * And if it is, it keeps reading only from the file
+             * With the white space character
+             */
             if(isspace(c1)){
                 readFromSecondFile = false;
                 continue;
@@ -118,6 +141,11 @@ int IsIdenticalFiles(const char* firstFilePath, const char* secondFilePath){
         }while (r1!=END_OF_FILE);
 
     }
+    // Close the files.
+    if(close(fd1)==SYSTEM_CALL_FAILURE || close(fd2)==SYSTEM_CALL_FAILURE){
+        EndProgram();
+    }
+
     if(different){
         return DIFFERENT;
     }
@@ -132,6 +160,6 @@ int main(int argc, char* argv[]) {
         printf(INPUT_ERROR);
         return ERROR;
     }
-    int result = IsIdenticalFiles(argv[FIRST_FILE_NAME],argv[SECOND_FILE_NAME]);
+    int result = Compare(argv[FIRST_FILE_NAME],argv[SECOND_FILE_NAME]);
     return result;
 }
